230b75b179319e438e08240c8b2b7e302cb4999e
trident.job <trident.job@0cf71ba8-db10-a7ce-79d2-7390b73a6746>
2011-08-08 21:05:36 +0000

    * Added Unit test with DUnit
    git-svn-id: https://delphi-code-coverage-wizard.googlecode.com/svn/trunk@14 0cf71ba8-db10-a7ce-79d2-7390b73a6746

---
+++ Test/DelphiCodeCoverageWizardTests.dpr
@@ -0,0 +1,35 @@
+program DelphiCodeCoverageWizardTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  Forms,
+  TestFramework,
+  GUITestRunner,
+  TextTestRunner,
+  Test_uProjectSettings in 'Test_uProjectSettings.pas',
+  uProjectSettings in '..\Source\uProjectSettings.pas';
+
+{R *.RES}
+
+begin
+  Application.Initialize;
+  if IsConsole then
+    with TextTestRunner.RunRegisteredTests do
+      Free
+  else
+    GUITestRunner.RunRegisteredTests;
+end.
+
