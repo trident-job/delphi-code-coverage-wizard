1c6c995342e31c11342a7034f1fd0d1759db853d
trident.job <trident.job@0cf71ba8-db10-a7ce-79d2-7390b73a6746>
2011-09-28 21:08:12 +0000

    Fixed daily build archive name
    git-svn-id: https://delphi-code-coverage-wizard.googlecode.com/svn/trunk@24 0cf71ba8-db10-a7ce-79d2-7390b73a6746

--- DailyBuild/DailyBuild_PreDeploy.bat
+++ DailyBuild/DailyBuild_PreDeploy.bat
@@ -2,4 +2,4 @@ rem #Create Dir(s)
 md tmp
 rem #Compress
 cd ..\Exe
-"C:\Program Files\7-Zip\7z.exe" a -tzip ..\dailybuild\tmp\DelphiCodeToDoc.zip @../DailyBuild/DailyBuild_FileList.txt -xr!*.svn -scsWIN
+"C:\Program Files\7-Zip\7z.exe" a -tzip ..\dailybuild\tmp\delphi-code-coverage-wizard.zip @../DailyBuild/DailyBuild_FileList.txt -xr!*.svn -scsWIN
